ea13935d8436cceefcea92f24605373d70f359da
[Rushikesh Dixit] <dixitrushikesh1999@gmail.com>
2022-02-25 16:09:18 +0530

    Changed CB Properties and test-script

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-import requests;
-url = "https://localhost:8082/api/response"
+import requests, time
+url = "http://localhost:8082/api/response"
 request_fallback = 0
-for i in range(100):
+N = 20
+for i in range(N):
     response = requests.get(url)
-    if response.status_code == 200 :
-        if response.json()["code"] == "500":
-            request_fallback += 1
-
-print(request_fallback/100)
-
+    service_statuscode = response.json()['code']
+    print("Service Status Code = {}".format(service_statuscode), "Time Taken = {} ms".format(response.elapsed.total_seconds()*1000))
+    if service_statuscode == 500:
+        request_fallback += 1
+    time.sleep(4)
+print("Fallback Percentage = {} %".format(request_fallback*100/N))
